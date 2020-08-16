4b27090834ba66307eb256782c19d30007e2791f
Or Tslil <orTslil64@gmail.com>
2020-08-16 10:01:53 +0300

    update

--- spo_dataset/spo_generator.py
+++ spo_dataset/spo_generator.py
@@ -233,4 +233,48 @@ def face_detection(video_path, frame_size = (256,256)):
     return np.array(state_images)
 
 
+def generate_dataset(n = 100,video_path = None, image_path = None, image_type = None, output_path = None, visualize = False, output_type = "images", output_folder = "dataset/images/dots/",  partial = False, mask = None):
+    frames = []
+    if mask is not None:
+        mask = cv2.imread(mask,0)
+        mask = cv2.resize(mask, (128,128),interpolation = cv2.INTER_AREA)
+    if video_path is not None:
+        images, _ = get_video(video_path, n)
+        x,z = get_dataset_from_video(images, n)
+    elif image_path is not None:
+        image = cv2.imread(image_path,0)
+        image = cv2.resize(image, (128,128),interpolation = cv2.INTER_AREA)
+        x,z = get_dataset_from_image(image/255, n, radius = [15, 25],  partial = partial, mask = mask)
+    elif image_type == "dots":
+        image = generate_image(0.01)
+        x,z = get_dataset_from_image(image, n, radius = [15, 25],  partial = partial, mask = mask)
+    elif image_type == "checkers":
+        image = np.array(data.checkerboard()).astype(np.float64)
+        image = cv2.resize(image, (128,128),interpolation = cv2.INTER_AREA)
+        x,z = get_dataset_from_image(image/255, n, radius = [15, 25],  partial = partial, mask = mask)
+    
+    for ii in range(n):
+        swirled  = z[ii]
+        state = x[ii]
+        if visualize:
+            cv2.imshow('swirled',swirled)
+            cv2.imshow('state',state)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+            time.sleep(0.01)
+        frame = np.concatenate((state,swirled),axis = 1)
+        if output_type == "video":
+            frames.append(frame*255)
+        elif output_type == "images":
+            fname = output_folder+str(ii)+".jpg"
+            skimage.io.imsave(fname,frame*255)
+    if visualize:
+        cv2.destroyAllWindows()
+        
+    if output_type == "video":
+        fname = output_folder+"dataset.mp4"
+        skvideo.io.vwrite(fname, frames) 
+        
+
     
